667b95cf95166f78472478606b65c5c574b31a10
sohsmt <tetsuwan.sou@gmail.com>
2020-11-18 06:32:43 +0900

    [update] header.scssの修正 [add] indexの作成

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
+    htmlAttrs: {
+      leng: 'ja',
+    },
     title: 'mosya3',
     meta: [
       { charset: 'utf-8' },
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,58 @@
 <template>
   <div class="main">
-    <div class="main__menu">
-      <div class="menu-head"></div>
-      <table border="1" width="100%" style="table-layout: auto">
-        <tr>
-          一般歯科
-        </tr>
-        <td>虫歯治療</td>
-        <td>歯周病治療</td>
-        <td>入れ歯</td>
-        <td>予防歯科</td>
-      </table>
-      <div class="menu-foot"></div>
+    <div class="menu">
+      <div class="menu-head">
+        <h2>一般歯科</h2>
+        <div class="menu-title">
+          <ul>
+            <li><a href="#">虫歯治療</a></li>
+            <li><a href="#">歯周病治療</a></li>
+            <li><a href="#">入れ歯</a></li>
+            <li><a href="#">予防歯科</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="menu-foot">
+        <h2>審美歯科</h2>
+        <div class="menu-title">
+          <ul>
+            <li><a href="#">ホワイトニング</a></li>
+            <li><a href="#">ホワイトニング</a></li>
+            <li><a href="#">ホワイトニング</a></li>
+            <li><a href="#">ホワイトニング</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
-    <div class="main__top">
-      <div class="top__img"></div>
-      <div class="top__infomation"></div>
+    <div class="main_top">
+      <div class="main__img">
+        <img src="~/assets/image/in01.jpg" />
+      </div>
+      <div class="main_info-wrap">
+        <h2>新着情報</h2>
+        <div class="news">
+          <div class="main_info">
+            <div class="main_info-time">
+              <p><time datetime="2012-08-01">2012年08月01日</time></p>
+            </div>
+            <div class="main_info-text">
+              <p>
+                【診療時間変更のお知らせ】長期休診の前日8月11日は、通常より1時間長く診療をすることにしました。ご予約をお待ちしております。
+              </p>
+            </div>
+          </div>
+          <div class="main_info">
+            <div class="main_info-time">
+              <p><time datetime="2012-07-24">2012年07月24日</time></p>
+            </div>
+            <div class="main_info-text">
+              <p>
+                【8月の休診のお知らせ】8月12日～16日の間は休診とさせていただきます。何卒よろしくお願いいたします。
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
