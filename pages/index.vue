61de5564f3bc1e747b83b62545161ac0a5430a0b
sohsmt <tetsuwan.sou@gmail.com>
2020-11-18 06:57:14 +0900

    [update] indexの修正　・marginの修正　・丸みを付ける

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="main">
     <div class="menu">
+      <h2>一般歯科</h2>
       <div class="menu-head">
-        <h2>一般歯科</h2>
         <div class="menu-title">
           <ul>
             <li><a href="#">虫歯治療</a></li>
@@ -12,8 +12,8 @@
           </ul>
         </div>
       </div>
+      <h2>審美歯科</h2>
       <div class="menu-foot">
-        <h2>審美歯科</h2>
         <div class="menu-title">
           <ul>
             <li><a href="#">ホワイトニング</a></li>
@@ -49,6 +49,34 @@
               <p>
                 【8月の休診のお知らせ】8月12日～16日の間は休診とさせていただきます。何卒よろしくお願いいたします。
               </p>
+            </div>
+          </div>
+          <div class="main_info">
+            <div class="main_info-time">
+              <p><time datetime="2012-07-02">2012年07月02日</time></p>
+            </div>
+            <div class="main_info-text">
+              <p>
+                【こどもデンタル教室のご案内】毎月第2土曜日に行っているこどもデンタル教室では、参加してくださるお子様を募集しております。
+              </p>
+            </div>
+          </div>
+          <div class="main_info">
+            <div class="main_info-time">
+              <p><time datetime="2012-06-20">2012年06月20日</time></p>
+            </div>
+            <div class="main_info-text">
+              <p>
+                【7月休診日のお知らせ】7月は土日祝日以外の休診日はありません。ご来院お待ちしております。
+              </p>
+            </div>
+          </div>
+          <div class="main_info">
+            <div class="main_info-time">
+              <p><time datetime="2012-06-01">2012年06月01日</time></p>
+            </div>
+            <div class="main_info-text">
+              <p>ホームページをリニューアルしました。</p>
             </div>
           </div>
         </div>
