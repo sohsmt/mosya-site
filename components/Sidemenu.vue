a40ad1d1bfeec2c865df6a116685ec61efa6c06d
sohsmt <tetsuwan.sou@gmail.com>
2020-11-18 18:28:56 +0900

    [update] サイドメニューをコンポーネント化

---
+++ components/Sidemenu.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="menu">
+    <h2>一般歯科</h2>
+    <div class="menu-head">
+      <div class="menu-title">
+        <ul>
+          <li><a href="#">虫歯治療</a></li>
+          <li><a href="#">歯周病治療</a></li>
+          <li><a href="#">入れ歯</a></li>
+          <li><a href="#">予防歯科</a></li>
+        </ul>
+      </div>
+    </div>
+    <h2>審美歯科</h2>
+    <div class="menu-foot">
+      <div class="menu-title">
+        <ul>
+          <li><a href="#">ホワイトニング</a></li>
+          <li><a href="#">オールセラミック</a></li>
+          <li><a href="#">セラミックインレー</a></li>
+          <li><a href="#">PMTC</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,6 @@
 <template>
   <div class="main">
-    <div class="menu">
-      <h2>一般歯科</h2>
-      <div class="menu-head">
-        <div class="menu-title">
-          <ul>
-            <li><a href="#">虫歯治療</a></li>
-            <li><a href="#">歯周病治療</a></li>
-            <li><a href="#">入れ歯</a></li>
-            <li><a href="#">予防歯科</a></li>
-          </ul>
-        </div>
-      </div>
-      <h2>審美歯科</h2>
-      <div class="menu-foot">
-        <div class="menu-title">
-          <ul>
-            <li><a href="#">ホワイトニング</a></li>
-            <li><a href="#">ホワイトニング</a></li>
-            <li><a href="#">ホワイトニング</a></li>
-            <li><a href="#">ホワイトニング</a></li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <Sidemenu />
     <div class="main_top">
       <div class="main__img">
         <img src="~/assets/image/in01.jpg" />
