2289573157dfac39b50fa0211633ec353aba3afb
sohsmt <tetsuwan.sou@gmail.com>
2020-11-22 09:03:50 +0900

    create　パンくずリストの作成

---
+++ components/Pankuzu.vue
@@ -0,0 +1,28 @@
+<template>
+  <ol class="pankuzu">
+    <li
+      itemprop="itemListElement"
+      itemscope
+      itemtype="https://schema.org/ListItem"
+    >
+      <nuxt-link itemprop="item" to="/">
+        <span itemprop="name">トップページ</span>
+      </nuxt-link>
+      <meta itemprop="position" content="1" />
+    </li>
+    <li
+      itemprop="itemListElement"
+      itemscope
+      itemtype="https://schema.org/ListItem"
+    >
+      <span itemprop="name">{{ linkname }}</span>
+      <meta itemprop="position" content="1" />
+    </li>
+  </ol>
+</template>
+
+<script>
+export default {
+  props: ['linkname'],
+}
+</script>
--- pages/clinic.vue
+++ pages/clinic.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="clinic">
-    <Sidemenu />
-    <div class="main-contents">
-      <h2></h2>
+    <Pankuzu linkname="医院紹介" />
+    <div class="clinic_main">
+      <Sidemenu />
+      <div class="main-contents_wrapper">
+        <h2>医院紹介</h2>
+        <div class="main-contents"></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      linkname: '',
+    }
+  },
+}
 </script>
