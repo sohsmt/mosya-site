1e1d0ff6bff71b3b0b0fbd5fdc837762dc7f49a1
sohsmt <tetsuwan.sou@gmail.com>
2020-11-23 07:45:56 +0900

    create 院長・スタッフ紹介

---
+++ pages/staff.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="staff">
+    <Pankuzu linkname="院長・スタッフ紹介" />
+    <div class="content_main">
+      <Sidemenu />
+      <div class="main-contents_wrapper">
+        <h2>院長・スタッフ紹介</h2>
+        <div class="main-contents">
+          <h3 class="mani_contents_title">院長紹介</h3>
+          <div class="column2_items">
+            <div class="column-left">
+              <img src="~/assets/image/st01.jpg" />
+              <h3><span class="mgn_l-15">院長</span>南 敬介</h3>
+            </div>
+            <div class="column-right">
+              <h3>院長略歴</h3>
+              <p>
+                1990年 神奈川県立医科歯科大学 卒業<br />
+                1991年 きたデンタルオフィス 勤務<br />
+                1993年 西野歯科医院 勤務<br />
+                1999年 東山歯科医院 勤務<br />
+                2002年 デンタルクリニック 勤務<br />
+                2008年 デンタルクリニック 院長に就任
+              </p>
+            </div>
+          </div>
+          <div class="text-box">
+            <h3 class="text-title">ご挨拶</h3>
+            <p>デンタルクリニック・院長の南敬介です。</p>
+            <p>
+              先代である父から続くこの医院は、2014年で40周年を迎えます。<br />
+              父の代から目標として掲げていたのが、”地域に根付いた歯科医院”であること。<br />
+              約5年前から始めた、小学生以下を対象とした「こどもデンタル教室」や、3ヶ月に一度行われる地域健康デーの無料診断など、少しでも地域に貢献できればと思い、スタッフやたくさんの方々の協力を得て続けることができています。
+            </p>
+            <p>
+              歯科治療の技術は年々進歩し続けています。<br />
+              ですが、患者さんの中にはそんなことを全くご存知ない方だって多くいらっしゃると思います。どんな技術や治療法も、来院してお話を聞いていただけないと知ってはもらえません。<br />
+              まずは「来院してもらう」こと。そして「知ってもらう」こと。<br />
+              これが当院のモットーです。
+            </p>
+          </div>
+
+          <!-- スタッフ紹介 -->
+          <h3 class="mani_contents_title">スタッフ紹介</h3>
+          <div class="text-box">
+            <img src="~/assets/image/st02.jpg" />
+            <p>
+              <span class="mgn_r-10">医師</span>近藤先生<br />
+              <span class="mgn_r-10">衛生士</span>芹沢先生<br />
+              <span class="mgn_r-10">衛生士</span>武田先生<br />
+              <span class="mgn_r-10">受付</span>沖田<br />
+              <span class="mgn_r-10">受付</span>山崎
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
