3687ae86c30c6fb146f44d6e7c3da3d6b360413e
sohsmt <tetsuwan.sou@gmail.com>
2020-11-23 11:04:55 +0900

    create アクセスの作成

---
+++ pages/access.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="access">
+    <Pankuzu linkname="アクセス" />
+    <div class="content_main">
+      <Sidemenu />
+      <div class="main-contents_wrapper">
+        <h2>アクセス</h2>
+        <div class="main-contents">
+          <h3 class="mani_contents_title">アクセスマップ</h3>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d25922.428317010545!2d139.71901511467652!3d35.694147352603984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188c5e412329bb%3A0x7db38e6732953dc!2z44CSMTYyLTA4NDYg5p2x5Lqs6YO95paw5a6_5Yy65biC6LC35bem5YaF55S677yS77yR4oiS77yR77yT!5e0!3m2!1sja!2sjp!4v1606090964306!5m2!1sja!2sjp"
+            width="600"
+            height="400"
+            frameborder="0"
+            style="border: 0"
+            allowfullscreen=""
+            aria-hidden="false"
+            tabindex="0"
+          ></iframe>
+          <a href="#" class="map_link"
+            ><p class="map_text">大きな地図で見る</p></a
+          >
+          <h3 class="mani_contents_title"></h3>
+          <div class="column2_items">
+            <div class="column-left">
+              <img src="~/assets/image/ac01.jpg" />
+            </div>
+            <div class="column-right">
+              <h3>デンタル&nbsp;クリニック</h3>
+              <p>東京都新宿区市谷左内町21-13</p>
+              <p>
+                JR総武線・都営地下鉄市ヶ谷駅方面から徒歩7分<br />
+                東京メトロ有楽町線・南北線市ヶ谷駅方面から徒歩5分
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
